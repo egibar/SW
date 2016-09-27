ea96bfb614e40cb93df70cdba3f8bde99c544306
Asier Eguibar <asien54@gmail.com>
2016-09-27 21:20:26 +0200

    js arreglado contraseña mas segura, select cambiado

--- ProyectoSW/funciones.js
+++ ProyectoSW/funciones.js
@@ -1,40 +1,135 @@
 /**
  * Created by egibar on 20/09/2016.
  */
-function revisar(elemento) {
+function revisarname(elemento) {
+    var ok = true;
+//    var nombre = document.getElementById('name');
     exp =/^[a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]/g;
-
     if (!exp.test(elemento.value)){
         elemento.className='error';
+        ok=false;
+        alert("El formato del nombre no es el correcto")
     }
     else elemento.className='form-input';
+    return ok;
 }
+
 function revisaremail(elemento) {
+    var ok = true;
     exp =/^[a-zA-Z]+\d{3}\@ikasle.ehu\.(eus|es)/g;
     if (!exp.test(elemento.value)) {
         elemento.className='error';
         alert("Error: La dirección de correo " + elemento.value + " es incorrecta.");
+        ok=false;
+
     }
     else elemento.className='form-input';
+
+    return ok;
 }
+
 function revisarpass(elemento) {
-//    exp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if( !(/^\d{6}$/.test(elemento.value)) ){
-        elemento.className='error';
+    var ok=true;
+    exp = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,15}/g;
+    if (!(exp.test(elemento.value))) {
+        elemento.className = 'error';
+        ok=false;
+        alert("la contraseña no tiene al menos 6 caracteres");
     }
-    else elemento.className='form-input';
+    else  elemento.className = 'form-input';
+
+    return ok;
 }
+
 function revisartel(elemento) {
-    valor = elemento.value;
-    if( !(/^\d{9}$/.test(valor)) ) {
+    exp=/^\d{9}$/;
+    ok=true;
+    if( !(exp.test(elemento.value)) ) {
         elemento.className='error';
+        ok=false;
         alert("Tiene que escribir un teléfono de 9 dígitos");
     }
-    else elemento.className='form-input';
+    else {elemento.className='form-input';
+        }
 }
-function revisaresp(elemento) {
-    if (elemento.selectedOptions==0){
-        elemento.className='error';
+
+function camposObigatorios(){
+    var ok = true;
+    var frm = document.getElementById("registro");
+    for(i=0;i<frm.elements.length - 4;i++){
+        var elem =frm.elements[i].value;
+        if(elem.length==0){
+            alert("No todos los campos obligatorios estan rellenos");
+            ok = false;
+        }
     }
-    else elemento.className='form-input';
+    return ok;
 }
+function revisaresp(obj){
+    if(obj.value=="Otra"){
+        var td = obj.parentNode;
+        var tr = td.parentNode;
+        tr.removeChild(td);
+       // document.getElementById("especialidad").removeChild(tr);
+        var textBox = document.createElement("input");
+        textBox.setAttribute("type", "text");
+        textBox.setAttribute("name", "especialidad");
+       // textBox.insertChildAfter(document.getElementById("especialidad"));
+        textBox.setAttribute("onblur", "revisaresp(this)");
+        var ntd = document.createElement("td");
+        ntd.appendChild(textBox);
+        tr.appendChild(ntd);
+    }else if(obj.value==""){
+        var td = obj.parentNode;
+        var tr = td.parentNode;
+        td.removeChild(obj);
+        var select = document.createElement("select");
+        select.setAttribute("name","especialidad");
+        select.setAttribute("onchange", "revisaresp(this)");
+        var is = document.createElement("option");
+        var istxt = document.createTextNode("Ingeniería del Software");
+        is.appendChild(istxt);
+        var ic = document.createElement("option");
+        var ictxt = document.createTextNode("Ingeniería de Computadores");
+        ic.appendChild(ictxt);
+        var c = document.createElement("option");
+        var ctxt = document.createTextNode("Computación");
+        c.appendChild(ctxt);
+        var o = document.createElement("option");
+        var otxt = document.createTextNode("Otra");
+        o.appendChild(otxt);
+        select.appendChild(is);
+        select.appendChild(ic);
+        select.appendChild(c);
+        select.appendChild(o);
+        td.appendChild(select);
+    }
+}
+
+
+function vervalores() {
+    var sAux = "";
+    var frm = document.getElementById("registro");
+    var nombre = document.getElementById('name');
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var telefono = document.getElementById("telefono");
+
+    revisarname(nombre);
+    revisaremail(email);
+    revisarpass(password);
+    revisartel(telefono);
+    revisaresp();
+    if (errorname & errormail & errorpass & errortel) {
+        for (i = 0; i < frm.elements.length - 2; i++) {
+            sAux += "NOMBRE: " + frm.elements[i].name + " ";
+            sAux += "VALOR: " + frm.elements[i].value + "\n";
+        }
+        alert(sAux);
+    }
+    else return false;
+}
+
+
+
+
