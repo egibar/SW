dd0ad42deb75f0906795bbb9c1d2332893bd2a7a
egibar <aeguibar006@ikasle.ehu.es>
2016-11-07 00:48:45 +0100

    ComprobarEmail

--- ProyectoSW/ComprobarEmail.php
+++ ProyectoSW/ComprobarEmail.php
@@ -8,7 +8,6 @@ require_once ('lib/class.wsdlcache.php');
 if(isset($_POST['email'])){
     $soapclient= new nusoap_client('http://sw14.hol.es/ServiciosWeb/comprobarmatricula.php?wsdl',true);
     $result=$soapclient->call('comprobar',array('x'=>$_POST['email']));
-    echo "<script type='text/javascript'>alert('$result');</script>";
     echo $result;
 
 }
